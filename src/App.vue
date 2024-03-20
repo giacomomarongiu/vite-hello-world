dd2245cc29cdcc031df4cd3ca0eed9c450aa25b7
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-20 15:51:48 +0100

    [Bonus Completed] New Component that can add a button ad a title on my app.vue. Deleted data() from app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,18 @@
 //Importo il mio altro file Vue "figlio" nel mio "genitore"
 import ButtonCounter from "./components/button.vue"
 export default {
-
-  data() {
-    return {
       //Per esporre il componente figlio importato al nostro modello,
       // dobbiamo registrarlo con il components 
       components: {
         ButtonCounter
       },
-      text: "Lunghe Vite a Vite!",
-    }
-  }
 }
 </script>
 
 <template>
-  <h1>{{ text }}</h1>
   <!--Ora posso usarlo come tag-->
-  <ButtonCounter></ButtonCounter>
+  <ButtonCounter>
+  </ButtonCounter>
 </template>
 
 <style></style>
--- src/components/button.vue
+++ src/components/button.vue
@@ -10,12 +10,14 @@
 export default {
     data() {
         return {
-            count: 0
+            count: 0,
+            text: "Lunghe Vite a Vite!",
         }
     }
 }
 </script>
 
 <template>
+    <h1>{{text}}</h1>
     <button @click="count++">You clicked me {{ count }} times.</button>
 </template>
