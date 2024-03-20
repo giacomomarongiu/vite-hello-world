e26ef55fa322e68d994afd521bc881834f5ff122
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-20 15:29:15 +0100

    [Bonus] Working on new component - Pause

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script>
+//Mi sembra di capire che qui piacciano le maiuscole
+//Importo il mio altro file Vue "figlio" nel mio "genitore"
+import ButtonCounter from "./components/button.vue"
 export default {
+
   data() {
     return {
-      text: "Hello Vite!",
+      //Per esporre il componente figlio importato al nostro modello,
+      // dobbiamo registrarlo con il components 
+      components: {
+        ButtonCounter
+      },
+      text: "Lunghe Vite a Vite!",
     }
   }
 }
 </script>
 
 <template>
-<h1>{{text}}</h1>
+  <h1>{{ text }}</h1>
+  <!--Ora posso usarlo come tag-->
+  <ButtonCounter></ButtonCounter>
 </template>
 
 <style></style>
---
+++ src/components/button.vue
@@ -0,0 +1,21 @@
+<!--Creo un nuovo file componente
+    Le componenti ci permettono di dividere la nostra Interfaccia in
+    diverse parti, riutilizzabili e pensate in modo "isolato". E' comune
+    immaginare una vue.app come "una specie di albero" in cui alcuni elementi
+    possono essere ripetuti (come in HTML) ma, la componente si porta dietro
+    con se una serie di logiche.
+-->
+<script>
+//Copio esempio Documetazione
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
